ffbcd8662fcdf97cb119a4e3a9f51f8ec697e185
Lucas Rossi <lucasrm2305@gmail.com>
2023-10-22 14:12:06 -0300

    add base character class

---
+++ Source/Aura/Aura.cpp
@@ -0,0 +1,6 @@
+// Copyright Lucas Rossi
+
+#include "Aura.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Aura, "Aura" );
---
+++ Source/Aura/Aura.h
@@ -0,0 +1,6 @@
+// Copyright Lucas Rossi
+
+#pragma once
+
+#include "CoreMinimal.h"
+
