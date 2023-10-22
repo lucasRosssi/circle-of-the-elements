989cd2cc3a821d3a76b65e97a04425fb2c3718ec
Lucas Rossi <lucasrm2305@gmail.com>
2023-10-22 19:28:46 -0300

    create main player controller class

---
+++ Source/Aura/Private/Player/MainPlayerController.cpp
@@ -0,0 +1,5 @@
+// Copyright Lucas Rossi
+
+
+#include "Player/MainPlayerController.h"
+
---
+++ Source/Aura/Public/Player/MainPlayerController.h
@@ -0,0 +1,17 @@
+// Copyright Lucas Rossi
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "MainPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API AMainPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
