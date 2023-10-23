f3c69b03da3201025cbfd284878d8167a65c2d9d
Lucas Rossi <lucasrm2305@gmail.com>
2023-10-23 20:59:11 -0300

    setup character movement parameters

--- Source/Aura/Private/Character/AuraCharacter.cpp
+++ Source/Aura/Private/Character/AuraCharacter.cpp
@@ -3,3 +3,17 @@
 
 #include "Character/AuraCharacter.h"
 
+#include "GameFramework/CharacterMovementComponent.h"
+
+AAuraCharacter::AAuraCharacter()
+{
+	UCharacterMovementComponent* Movement = GetCharacterMovement();
+	Movement->bOrientRotationToMovement = true;
+	Movement->RotationRate = FRotator(0.f, 400.f, 0.f);
+	Movement->bConstrainToPlane = true;
+	Movement->bSnapToPlaneAtStart = true;
+
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationRoll = false;
+	bUseControllerRotationYaw = false;
+}
