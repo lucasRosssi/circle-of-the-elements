e720712cf8a9b9abc6437f0c87f1e8a12e3ff697
Lucas Rossi <lucasrm2305@gmail.com>
2023-11-02 10:45:24 -0300

    finish highlighting enemy

--- Source/Aura/Public/Character/AuraEnemy.h
+++ Source/Aura/Public/Character/AuraEnemy.h
@@ -15,9 +15,8 @@ class AURA_API AAuraEnemy : public AAuraCharacterBase, public ITargetInterface
 {
 	GENERATED_BODY()
 public:
+	AAuraEnemy();
+	
 	virtual void HighlightActor() override;
 	virtual void UnHighlightActor() override;
-
-	UPROPERTY(BlueprintReadOnly)
-	bool bHighlighted = false;
 };
