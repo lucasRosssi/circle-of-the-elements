6305cb6f291e82a396c365e487aff1c53c74ef12
lucasRosssi <lucasrm2305@gmail.com>
2024-09-26 10:03:28 -0300

    create UpgradeManager

---
+++ Source/Aura/Public/Managers/UpgradeManager.h
@@ -0,0 +1,17 @@
+// Copyright Lucas Rossi
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AuraSystemComponent.h"
+#include "UpgradeManager.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UUpgradeManager : public UAuraSystemComponent
+{
+	GENERATED_BODY()
+	
+};
