5cda2f45be0aba47cfa3a710d720c2354fe9e363
Lucas Rossi <lucasrm2305@gmail.com>
2024-09-26 08:41:59 -0300

    fix identation

--- Source/Aura/Private/Game/Components/RewardManager.cpp
+++ Source/Aura/Private/Game/Components/RewardManager.cpp
@@ -30,50 +30,52 @@ void URewardManager::BeginPlay()
 
 FRewardInfo URewardManager::GetRewardInfo(const FGameplayTag& RewardTag)
 {
-	return GetAuraGameMode()->RewardsInfo->GetRewardInfo(RewardTag);
+  return GetAuraGameMode()->RewardsInfo->GetRewardInfo(RewardTag);
 }
 
 void URewardManager::SetGatesRewards()
 {
-	TArray<AActor*> GateActors;
-	UGameplayStatics::GetAllActorsOfClass(
-		GetOwner(),
-		AGate::StaticClass(),
-		GateActors
-		);
+  TArray<AActor*> GateActors;
+  UGameplayStatics::GetAllActorsOfClass(
+    GetOwner(),
+    AGate::StaticClass(),
+    GateActors
+  );
 
-	TArray<FGameplayTag> SelectedRewards;
-	TArray<FGameplayTag> ReturningRewards;
-	for (const auto GateActor : GateActors)
-	{
-		FGameplayTag NextReward = GetNextRewardInBag();
+  TArray<FGameplayTag> SelectedRewards;
+  TArray<FGameplayTag> ReturningRewards;
+  for (const auto GateActor : GateActors)
+  {
+    FGameplayTag NextReward = GetNextRewardInBag();
 
-	  int32 Count = 0; // Add a Count to get out of the while loop in some rare situations. Most of the time the loop will finish before Count reaching 5.
-		while (SelectedRewards.Contains(NextReward) && Count < 5)
-		{
-			ReturningRewards.Add(NextReward);
-			NextReward = GetNextRewardInBag();
-		  Count++;
-		}
+    int32 Count = 0;
+    // Add a Count to get out of the while loop in some rare situations. Most of the time the loop will finish before Count reaching 5.
+    while (SelectedRewards.Contains(NextReward) && Count < 5)
+    {
+      ReturningRewards.Add(NextReward);
+      NextReward = GetNextRewardInBag();
+      Count++;
+    }
 
-	  AGate* Gate = Cast<AGate>(GateActor);
+    AGate* Gate = Cast<AGate>(GateActor);
 
-		if (!Gate) continue;
-		
-	  if (Count == 5 && SelectedRewards.Contains(NextReward)) // If it was the last count and the NextReward is still one already assigned to another gate, we deactivate this gate.
-	  {
-	    Gate->DeactivateGate();
-	  }
-		
+    if (!Gate) continue;
+
+    if (Count == 5 && SelectedRewards.Contains(NextReward))
+    // If it was the last count and the NextReward is still one already assigned to another gate, we deactivate this gate.
+    {
+      Gate->DeactivateGate();
+    }
+
     if (Gate->IsActive())
     {
       Gate->SetGateReward(NextReward);
-		  SelectedRewards.Add(NextReward);
+      SelectedRewards.Add(NextReward);
     }
-	}
+  }
 
   if (ReturningRewards.IsEmpty()) return;
-  
+
   for (const auto& Reward : ReturningRewards)
   {
     RewardBag.Add(Reward);
@@ -82,42 +84,42 @@ void URewardManager::SetGatesRewards()
 
 void URewardManager::SpawnReward()
 {
-	const ACharacter* PlayerCharacter = UGameplayStatics::GetPlayerCharacter(GetOwner(), 0);
-	FTransform Transform;
-	FVector RandomDirection = UKismetMathLibrary::RandomUnitVector();
-	RandomDirection.Z = 0.f;
-	FVector PlayerLocation = PlayerCharacter->GetActorLocation();
-	PlayerLocation.Z -= PlayerCharacter->GetCapsuleComponent()->GetScaledCapsuleHalfHeight();
-	const FVector SpawnLocation = PlayerLocation + RandomDirection * 200.f;
-	Transform.SetLocation(SpawnLocation);
-	
-	const FRewardInfo& Info = GetNextRewardInfo();
-	
-	ALocationReward* Reward = GetWorld()->SpawnActorDeferred<ALocationReward>(
-		Info.RewardClass,
-		Transform,
-		nullptr,
-		nullptr,
-		ESpawnActorCollisionHandlingMethod::AdjustIfPossibleButAlwaysSpawn
-		);
-	
-	if (Reward)
-	{
-		Transform.SetLocation(FVector(
-			SpawnLocation.X,
-			SpawnLocation.Y,
-			PlayerLocation.Z + Reward->GetInteractAreaRadius()));
-		Reward->FinishSpawning(Transform);
-	}
-	else
-	{
-		UE_LOG(
-			LogAura,
-			Error,
-			TEXT("Failed to spawn Location Reward: %s"),
-			*Info.RewardClass->GetName()
-			);
-	}
+  const ACharacter* PlayerCharacter = UGameplayStatics::GetPlayerCharacter(GetOwner(), 0);
+  FTransform Transform;
+  FVector RandomDirection = UKismetMathLibrary::RandomUnitVector();
+  RandomDirection.Z = 0.f;
+  FVector PlayerLocation = PlayerCharacter->GetActorLocation();
+  PlayerLocation.Z -= PlayerCharacter->GetCapsuleComponent()->GetScaledCapsuleHalfHeight();
+  const FVector SpawnLocation = PlayerLocation + RandomDirection * 200.f;
+  Transform.SetLocation(SpawnLocation);
+
+  const FRewardInfo& Info = GetNextRewardInfo();
+
+  ALocationReward* Reward = GetWorld()->SpawnActorDeferred<ALocationReward>(
+    Info.RewardClass,
+    Transform,
+    nullptr,
+    nullptr,
+    ESpawnActorCollisionHandlingMethod::AdjustIfPossibleButAlwaysSpawn
+  );
+
+  if (Reward)
+  {
+    Transform.SetLocation(FVector(
+      SpawnLocation.X,
+      SpawnLocation.Y,
+      PlayerLocation.Z + Reward->GetInteractAreaRadius()));
+    Reward->FinishSpawning(Transform);
+  }
+  else
+  {
+    UE_LOG(
+      LogAura,
+      Error,
+      TEXT("Failed to spawn Location Reward: %s"),
+      *Info.RewardClass->GetName()
+    );
+  }
 }
 
 void URewardManager::RemoveRewardFromPool(const FGameplayTag& RewardTag)
@@ -132,45 +134,46 @@ void URewardManager::RemoveRewardFromPool(const FGameplayTag& RewardTag)
 
 FGameplayTag URewardManager::GetNextRewardInBag()
 {
-	if (RewardBag.IsEmpty())
-	{
-		FillAndShuffleRewardBag();
-	}
+  if (RewardBag.IsEmpty())
+  {
+    FillAndShuffleRewardBag();
+  }
 
-	return RewardBag.Pop();
+  return RewardBag.Pop();
 }
 
 void URewardManager::FillAndShuffleRewardBag()
 {
-	if (bOverrideReward)
-	{
-	  if (
-	    OverridenRewardBag.Num() > 0 &&
+  if (bOverrideReward)
+  {
+    if (
+      OverridenRewardBag.Num() > 0 &&
       OverridenRewardBag[OverridenRewardBag.Num() - 1].MatchesTag(FAuraGameplayTags::Get().Resources)
-      )
-	  {
-		  RewardBag = OverridenRewardBag;
-	  }
-	  else
-	  {
-	    UE_LOG(LogAura, Warning, TEXT("Trying to override Reward Bag with invalid data! Falling back to randomizing items."))
-	    GetAuraGameMode()->RewardsInfo->FillRewardBag(RewardBag, BlockedRewards);
-	    Algo::RandomShuffle(RewardBag);
-	  }
-	}
-	else
-	{
-		GetAuraGameMode()->RewardsInfo->FillRewardBag(RewardBag, BlockedRewards);
-		Algo::RandomShuffle(RewardBag);
-	}
+    )
+    {
+      RewardBag = OverridenRewardBag;
+    }
+    else
+    {
+      UE_LOG(LogAura, Warning,
+             TEXT("Trying to override Reward Bag with invalid data! Falling back to randomizing items."))
+      GetAuraGameMode()->RewardsInfo->FillRewardBag(RewardBag, BlockedRewards);
+      Algo::RandomShuffle(RewardBag);
+    }
+  }
+  else
+  {
+    GetAuraGameMode()->RewardsInfo->FillRewardBag(RewardBag, BlockedRewards);
+    Algo::RandomShuffle(RewardBag);
+  }
 }
 
 FRewardInfo URewardManager::GetNextRewardInfo()
 {
-	if (!NextRewardTag.IsValid())
-	{
-		NextRewardTag = GetNextRewardInBag();
-	}
-	
-	return GetAuraGameMode()->RewardsInfo->GetRewardInfo(NextRewardTag);
+  if (!NextRewardTag.IsValid())
+  {
+    NextRewardTag = GetNextRewardInBag();
+  }
+
+  return GetAuraGameMode()->RewardsInfo->GetRewardInfo(NextRewardTag);
 }
