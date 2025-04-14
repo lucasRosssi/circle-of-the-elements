ee9d714391a9d75f4e024835b7a748e51a4bee6a
lucasRosssi <lucasrm2305@gmail.com>
2025-04-14 19:51:59 -0300

    reorganize widgets folders

---
+++ Source/Aura/Public/UI/WidgetController/LoadoutWidgetController.h
@@ -0,0 +1,17 @@
+// Copyright Lucas Rossi
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/WidgetController/AuraWidgetController.h"
+#include "LoadoutWidgetController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API ULoadoutWidgetController : public UAuraWidgetController
+{
+	GENERATED_BODY()
+	
+};
