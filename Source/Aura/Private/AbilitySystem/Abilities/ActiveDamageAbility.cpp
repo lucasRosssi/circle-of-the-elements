df02e13aac7e41b806d161d62d96376229dc8811
lucasRosssi <lucasrm2305@gmail.com>
2025-04-20 10:25:11 -0300

    change abilities animations / fix melee combo not reseting combo window state when ending ability

--- Source/Aura/Private/AbilitySystem/Abilities/ActiveDamageAbility.cpp
+++ Source/Aura/Private/AbilitySystem/Abilities/ActiveDamageAbility.cpp
@@ -9,97 +9,99 @@
 
 FAbilityParams UActiveDamageAbility::MakeAbilityParamsFromDefaults(AActor* TargetActor) const
 {
-	float CurrentDamage = GetDamageAtLevel(GetAbilityLevel());
-	FAbilityParams AbilityParams = Super::MakeAbilityParamsFromDefaults(TargetActor);
-	AbilityParams.DamageParams.DamageEffectClass = DamageEffectClass;
-	AbilityParams.DamageParams.DamageType = DamageType;
-	AbilityParams.DamageParams.bApplyHitReact = bApplyHitReact;
-	
-	if (bIsComboSequence)
-	{
-		if (ComboIndex == ComboSequenceData.Num() - 1)
-		{
-			const float FinishEffectChange = ComboFinishMagnitude.GetValueAtLevel(GetAbilityLevel());
-			CurrentDamage = CurrentDamage * (1.f + FinishEffectChange);
-		}
-		else
-		{
-			const float EffectChange = ComboMagnitude.GetValueAtLevel(GetAbilityLevel());
-			CurrentDamage = CurrentDamage * (1.f + EffectChange * ComboIndex);
-		}
-	}
-	
-	AbilityParams.DamageParams.BaseDamage = CurrentDamage;
-	
-	
-	return AbilityParams;
+  float CurrentDamage = GetDamageAtLevel(GetAbilityLevel());
+  FAbilityParams AbilityParams = Super::MakeAbilityParamsFromDefaults(TargetActor);
+  AbilityParams.DamageParams.DamageEffectClass = DamageEffectClass;
+  AbilityParams.DamageParams.DamageType = DamageType;
+  AbilityParams.DamageParams.bApplyHitReact = bApplyHitReact;
+
+  if (bIsComboSequence)
+  {
+    if (ComboIndex == ComboSequenceData.Num() - 1)
+    {
+      const float FinishEffectChange = ComboFinishMagnitude.GetValueAtLevel(GetAbilityLevel());
+      CurrentDamage = CurrentDamage * (1.f + FinishEffectChange);
+    }
+    else
+    {
+      const float EffectChange = ComboMagnitude.GetValueAtLevel(GetAbilityLevel());
+      CurrentDamage = CurrentDamage * (1.f + EffectChange * ComboIndex);
+    }
+  }
+
+  AbilityParams.DamageParams.BaseDamage = CurrentDamage;
+
+
+  return AbilityParams;
 }
 
 void UActiveDamageAbility::EndAbility(
-	const FGameplayAbilitySpecHandle Handle,
-	const FGameplayAbilityActorInfo* ActorInfo,
-	const FGameplayAbilityActivationInfo ActivationInfo,
-	bool bReplicateEndAbility,
-	bool bWasCancelled)
+  const FGameplayAbilitySpecHandle Handle,
+  const FGameplayAbilityActorInfo* ActorInfo,
+  const FGameplayAbilityActivationInfo ActivationInfo,
+  bool bReplicateEndAbility,
+  bool bWasCancelled
+)
 {
-	Super::EndAbility(Handle, ActorInfo, ActivationInfo, bReplicateEndAbility, bWasCancelled);
+  Super::EndAbility(Handle, ActorInfo, ActivationInfo, bReplicateEndAbility, bWasCancelled);
 
-	ComboIndex = 0;
+  ComboIndex = 0;
   bComboInputPressed = false;
+  bInComboWindow = false;
 }
 
 bool UActiveDamageAbility::IsDamageAbility_Implementation() const
 {
-	return true;
+  return true;
 }
 
 FGameplayTag UActiveDamageAbility::GetDamageTypeTag_Implementation() const
 {
-	return DamageType;
+  return DamageType;
 }
 
 FAbilityParams UActiveDamageAbility::ApplyEffectChangePerHitToAbilityParams(
-	FAbilityParams& AbilityParams,
-	int32 HitCount
-	)
+  FAbilityParams& AbilityParams,
+  int32 HitCount
+)
 {
-	if (RangedHitMode != EAbilityHitMode::Default && GetMaxHitCount() > 1)
-	{
-		AbilityParams.DamageParams.BaseDamage *= (1.f + GetEffectChangePerHit() * HitCount);
-	  if (AbilityParams.EffectParams.Num() > 0)
-	  {
-	    for (auto& StatusEffect : AbilityParams.EffectParams)
-	    {
-		    StatusEffect.Value *= (1.f + GetEffectChangePerHit() * HitCount);
-		    StatusEffect.Duration *= (1.f + GetEffectChangePerHit() * HitCount);
-	    }
-	  }
-	}
+  if (RangedHitMode != EAbilityHitMode::Default && GetMaxHitCount() > 1)
+  {
+    AbilityParams.DamageParams.BaseDamage *= (1.f + GetEffectChangePerHit() * HitCount);
+    if (AbilityParams.EffectParams.Num() > 0)
+    {
+      for (auto& StatusEffect : AbilityParams.EffectParams)
+      {
+        StatusEffect.Value *= (1.f + GetEffectChangePerHit() * HitCount);
+        StatusEffect.Duration *= (1.f + GetEffectChangePerHit() * HitCount);
+      }
+    }
+  }
 
-	return AbilityParams;
+  return AbilityParams;
 }
 
 float UActiveDamageAbility::GetDamageAtLevel(int32 Level) const
 {
-	return Damage.GetValueAtLevel(Level);
+  return Damage.GetValueAtLevel(Level);
 }
 
 int32 UActiveDamageAbility::GetRoundedDamageAtLevel_Implementation(int32 Level) const
 {
-	return Damage.AsInteger(Level);
+  return Damage.AsInteger(Level);
 }
 
 void UActiveDamageAbility::HandleComboSequence()
 {
-	if (bIsPlayerAbility && (!bComboInputPressed || !bInComboWindow)) return;
+  if (bIsPlayerAbility && (!bComboInputPressed || !bInComboWindow)) return;
 
-	ComboIndex += 1;
-	bComboInputPressed = false;
+  ComboIndex += 1;
+  bComboInputPressed = false;
   bInComboWindow = false;
 
-	if (!ComboSequenceData.IsValidIndex(ComboIndex)) return;
+  if (!ComboSequenceData.IsValidIndex(ComboIndex)) return;
 
-	OnNextComboSequence();
+  OnNextComboSequence();
 }
 
 UAnimMontage* UActiveDamageAbility::GetAnimMontage() const
@@ -119,12 +121,12 @@ bool UActiveDamageAbility::IsUsingWeapon() const
 
 float UActiveDamageAbility::GetComboMagnitude() const
 {
-	return ComboMagnitude.GetValueAtLevel(GetAbilityLevel());
+  return ComboMagnitude.GetValueAtLevel(GetAbilityLevel());
 }
 
 float UActiveDamageAbility::GetComboFinishMagnitude() const
 {
-	return ComboFinishMagnitude.GetValueAtLevel(GetAbilityLevel());
+  return ComboFinishMagnitude.GetValueAtLevel(GetAbilityLevel());
 }
 
 FComboData UActiveDamageAbility::GetComboData() const
